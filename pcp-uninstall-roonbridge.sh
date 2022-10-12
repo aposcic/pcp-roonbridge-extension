82bd3149d94d80d5c758225a22a042fd5c6fe7b5
aposcic <antonio.poscic@gmail.com>
2022-10-12 20:39:26 +0200

    Fix uninstaller file list cleanup

--- pcp-uninstall-roonbridge.sh
+++ pcp-uninstall-roonbridge.sh
@@ -18,18 +18,20 @@ sudo -u tc tce-audit delete $EXTENSION_NAME
 # clear the user command
 pcp_write_var_to_config USER_COMMAND_1 ""
 
-# remove files from piCore backup/persistence
+# remove directores from piCore backup/persistence
 sed -i '/var\/roon\/RoonBridge/d' /opt/.filetool.lst
 sed -i '/var\/roon\/RAATServer/d' /opt/.filetool.lst
+
+# remove directories from piCore exclusion list
+sed -i '/var\/roon\/RoonBridge\/Logs/d' /opt/.xfiletool.lst
+sed -i '/var\/roon\/RAATServer\/Logs/d' /opt/.xfiletool.lst
+sed -i '/opt\/RoonBridge/d' /opt/.xfiletool.lst
+
+# remove Roon Bridge dist directory so that it doesn't get persisted
+rm -r /opt/RoonBridge
 
 # back up changes to SD card
 echo "[pcp-uninstall-roonbridge] Backing up data to SD card..."
 filetool.sh -b
-
-# remove files from piCore exclusion list
-# (this needs to happen AFTER backing up so that the excluded files don't get persisted)
-sed -i '/var\/roon\/RoonBridge\/Logs/d' /opt/.xfiletool.lst
-sed -i '/var\/roon\/RAATServer\/Logs/d' /opt/.xfiletool.lst
-sed -i '/opt\/RoonBridge/d' /opt/.xfiletool.lst
 
 echo "[pcp-uninstall-roonbridge] Roon Bridge uninstallation finished. Please restart your system."
